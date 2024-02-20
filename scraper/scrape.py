1db126dec44b9bcf0d5ce266d18e8bf2575eb383
temi-oye <29523551+temi-oye@users.noreply.github.com>
2024-02-20 17:57:04 +0000

    move image bytes logic

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -22,31 +22,33 @@ adidas_src_url_pattern = r"https://assets.adidas.com/images/w_\d+,h_\d+"
 pintest_src_url_pattern = r"https://i.pinimg.com/\d+x/*"
 
 
+def get_image_bytes(src: str):
+    if re.match(r"https:*", src):
+        if re.match(adidas_src_url_pattern, src):
+            new_width = random.randint(400, 1000)  # nosec
+            new_height = random.randint(400, 1000)  # nosec
+            src = re.sub(adidas_src_url_pattern, f"https://assets.adidas.com/images/w_{new_width},h_{new_height}", src)
+        elif re.match(pintest_src_url_pattern, src):
+            src = re.sub(pintest_src_url_pattern, "https://i.pinimg.com/564x/", src)
+
+        # wait in between requests
+        sleep(random.random() * 5.3 + 0.8)  # nosec
+        return requests.get(url=src, timeout=5).content
+
+    elif re.match(base64_encoded_image_pattern, src):
+        base64_encoding = re.sub("data:image/(jpeg|png);base64,", "", src)
+        return b64decode(base64_encoding)
+
+
 def download_images(img_sources: ResultSet[Any], company: str, num_scraped_images_for_current_company: int) -> None:
     for img in img_sources:
-        src = img.get("src")
+        src: str = img.get("src")
         if not src:
             continue
 
         img_path = f"{company_images_dir}/{company}_img_{str(num_scraped_images_for_current_company).zfill(3)}"
 
-        img_bytes: bytes
-        if re.match(r"https:*", src):
-            if re.match(adidas_src_url_pattern, src):
-                new_width = random.randint(400, 1000)  # nosec
-                new_height = random.randint(400, 1000)  # nosec
-                src = re.sub(
-                    adidas_src_url_pattern, f"https://assets.adidas.com/images/w_{new_width},h_{new_height}", src
-                )
-            elif re.match(pintest_src_url_pattern, src):
-                src = re.sub(pintest_src_url_pattern, "https://i.pinimg.com/564x/", src)
-            img_bytes = requests.get(url=src, timeout=5).content
-            # wait in between requests
-            sleep(random.random() * 5.3 + 0.8)  # nosec
-
-        elif re.match(base64_encoded_image_pattern, src):
-            base64_encoding = re.sub("data:image/(jpeg|png);base64,", "", src)
-            img_bytes = b64decode(base64_encoding)
+        img_bytes = get_image_bytes(src)
 
         print("img_src: ", src)
 
