2a9ffcec4ce1406e83eaef933dc7cc78ce2363cf
Sam <sktylr@gmail.com>
2024-02-27 23:04:27 +0000

    add register user endpoint

---
+++ api/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+app_name = "accounts"
+
+router = DefaultRouter()
+
+urlpatterns = router.urls
+
+urlpatterns.extend(
+    [
+        path("register/", views.RegisterViewSet.as_view(), name="register"),
+    ]
+)
