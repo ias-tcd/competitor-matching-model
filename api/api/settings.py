3aea320c87f2d1d69fe0f886a1b1011dc9aadba9
Sam <sktylr@gmail.com>
2024-02-27 15:13:19 +0000

    add custom django command to run script in django context

--- api/api/settings.py
+++ api/api/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "images.apps.ImagesConfig",
+    "api.apps.APIConfig",
 ]
 
 MIDDLEWARE = [
--- ml/models/logo_detection/process.py
+++ ml/models/logo_detection/process.py
@@ -1,8 +1,10 @@
 import torch
+
 from .detect import Detector
 
 
-def process(input_image_path: str, model_path: str = "/src/ml/models/logo_detection"):
+def process(input_image_path: str):
+    model_path = "/src/ml/models/logo_detection"
     source = input_image_path
     detector = Detector(weights=f"{model_path}/logo_detection.pt", source=source)
 
