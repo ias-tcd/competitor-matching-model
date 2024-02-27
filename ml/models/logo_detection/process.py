4f75568d0a9ef5afb619a8235476fd4d37a5b5bc
Peng Hui Liu <127897896+liuphui@users.noreply.github.com>
2024-02-27 12:39:32 +0000

    Added model for detection and processing (#80)
    * Added model for detection and processing
    
    * Removed and added changes
    
    * Commented out and removed code to pass make lint
    
    * Uncomment __init__.py
    
    ---------
    
    Co-authored-by: Sam <sktylr@gmail.com>

---
+++ ml/models/logo_detection/process.py
@@ -0,0 +1,12 @@
+import torch
+from .detect import Detector
+
+
+def process(input_image_path: str, model_path: str = "/src/ml/models/logo_detection"):
+    source = input_image_path
+    detector = Detector(weights=f"{model_path}/logo_detection.pt", source=source)
+
+    with torch.no_grad():
+        inferences = detector.detect()
+
+    return inferences
