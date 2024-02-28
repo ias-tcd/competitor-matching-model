981f55a5a991fba2f20fbbff958fc867347149ea
Sam <sktylr@gmail.com>
2024-02-28 19:36:30 +0000

    add filter to limit queryset to only user owned objects by default

--- api/api/settings.py
+++ api/api/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 
 import dj_database_url
 
+from . import filters
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -194,6 +196,7 @@ REST_FRAMEWORK = {
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
+        filters.IsObjectOwnerFilter,
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
