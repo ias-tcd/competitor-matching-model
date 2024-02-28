bae090cf85d815a03c2d28ee26ec03cacc290399
Sam <sktylr@gmail.com>
2024-02-28 19:39:29 +0000

    add permission to restrict object access to users

--- api/api/settings.py
+++ api/api/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 import dj_database_url
 
-from . import filters
+from . import filters, permissions
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -190,6 +190,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        permissions.HasObjectOwnerPermission,
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_FILTER_BACKENDS": (
