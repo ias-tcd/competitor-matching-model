267aa2fb640163bca3b6299776c18e6ae5447137
Dhruv Ranajit Choudhury <111229637+dhrvrc@users.noreply.github.com>
2024-03-01 16:15:03 +0000

    Linked Images endpoint to the Model (#90)
    * Updating api branch to include model (#85)
    
    * add nginx configuration and deployment script
    
    * update deployment script and nginx configuration as well as adding deplyoment information to README
    
    * add more urls and prevent overwriting images issue
    
    * enable scraping for base64 encoded images
    
    * move company urls to separate file + small refector
    
    * refactor code
    
    * collect adidas images
    
    * add correct file extension to images
    
    * restore original images
    
    * move image bytes logic
    
    * save image with file extension
    
    * remove unuseful images
    
    * rename some images
    
    * comment out urls used
    
    * Feat: Update docker building for model requirements (#69)
    
    update docker building with new requirements and rename models directory to avoid conflicts
    
    * add flake8 configuration to ignore unused imports from __init__.py
    
    * add make_temp_directory helper for temporarily saving files
    
    * Added 'images' app (#70)
    
    * Chore: Bump Django and OpenCV versions (#75)
    
    * bump django version to avoid vulnerabilities
    
    * bump open cv version to avoid vulnerabilities
    
    * collect under armour images (#72)
    
    * Bugfix: Update deployment script for larger docker images (#78)
    
    * force remove dangling docker resources and remove call to pull images before building
    
    * add index url to install CPU version of torch by prebuilt wheel
    
    * combine environment instructions and add all unnecessary files to docker ignore
    
    * Update README with CI/CD status badges and database migration instructions (#79)
    
    add ci status badges and update installation instructions to migrate
    
    * Added model for detection and processing (#80)
    
    * Added model for detection and processing
    
    * Removed and added changes
    
    * Commented out and removed code to pass make lint
    
    * Uncomment __init__.py
    
    ---------
    
    Co-authored-by: Sam <sktylr@gmail.com>
    
    ---------
    
    Co-authored-by: Sam <sktylr@gmail.com>
    Co-authored-by: temi-oye <29523551+temi-oye@users.noreply.github.com>
    Co-authored-by: Peng Hui Liu <127897896+liuphui@users.noreply.github.com>
    
    * Initial attempt to link model to endpoint
    
    * Trying to fix failures and changing variable and method names to match rest of the repository
    
    * Linked endpoint to the model (Hopefully)
    
    * Fixed the no 400 error code issue
    
    * Fix spelling error
    
    * fixed issue of writing image to temporary directory
    
    * NoChangesAddedComment
    
    * Removed redundant comments
    
    * Removed redundant comments
    
    * Formatting changes to adhere to snake_case convention and removing redundant printing
    
    * More snake_case changes
    
    * Naming convention changes
    
    ---------
    
    Co-authored-by: Sam <sktylr@gmail.com>
    Co-authored-by: temi-oye <29523551+temi-oye@users.noreply.github.com>
    Co-authored-by: Peng Hui Liu <127897896+liuphui@users.noreply.github.com>
    Co-authored-by: egaynor <egaynor@tcd.ie>

---
+++ api/images/services/logo_detection_service.py
@@ -0,0 +1,19 @@
+from ml.models.logo_detection.process import process as detect_logos
+
+# from rest_framework import serializers
+
+
+class LogoDetectionService:
+    def detect_in_image(self, image_path):
+        inference_results = []
+        inferences = detect_logos(image_path)  # List LogoDetectionInference objects (ml.models.logo_detection.data)
+        for inference in inferences:
+            bbox = {
+                "x": inference.bounding_box.x,
+                "y": inference.bounding_box.y,
+                "w": inference.bounding_box.w,
+                "h": inference.bounding_box.h,
+            }
+            detection = {"bbox": bbox, "confidence": inference.confidence}
+            inference_results.append(detection)
+        return inference_results
