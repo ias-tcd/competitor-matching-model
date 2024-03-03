eaeffe40cc56dc6b73d9a2323ca40bc1b2a2e1f5
Sam <sktylr@gmail.com>
2024-03-03 23:04:04 +0000

    refactor payload creation in register viewset tests

--- api/tests/accounts/views/test_register_viewset_IT.py
+++ api/tests/accounts/views/test_register_viewset_IT.py
@@ -9,15 +9,7 @@ User = get_user_model()
 
 class TestRegisterViewSetIT(IntegrationTestCase):
     def test_register(self):
-        response = self._register(
-            {
-                "first_name": "Test",
-                "last_name": "Name",
-                "email": "test@example.com",
-                "password": "Password123!",
-                "confirm_password": "Password123!",
-            }
-        )
+        response = self._register()
         self.assertEqual(response.status_code, 201)
         created_user = User.objects.get(email="test@example.com")
         self.assertEqual(created_user.first_name, "Test")
@@ -25,93 +17,64 @@ class TestRegisterViewSetIT(IntegrationTestCase):
         self.assertEqual(created_user.username, "test@example.com")
 
     def test_hashes_password(self):
-        response = self._register(
-            {
-                "first_name": "Test",
-                "last_name": "Name",
-                "email": "test@example.com",
-                "password": "Password123!",
-                "confirm_password": "Password123!",
-            }
-        )
+        response = self._register()
         self.assertEqual(response.status_code, 201)
         created_user = User.objects.get(email="test@example.com")
         self.assertNotEqual(created_user.password, "Password123!")
 
     def test_password_matching_confirm_validation(self):
-        response = self._register(
-            {
-                "first_name": "Test",
-                "last_name": "Name",
-                "email": "test@example.com",
-                "password": "Password123!",
-                "confirm_password": "NotPassword123!",
-            }
-        )
+        response = self._register(confirm_password="NotPassword123!")
         self.assertEqual(response.status_code, 400)
         self.assertFalse(User.objects.filter(email="test@example.com").exists())
 
     def test_email_validation(self):
-        response = self._register(
-            {
-                "first_name": "Test",
-                "last_name": "Name",
-                "email": "email",
-                "password": "Password123!",
-                "confirm_password": "Password123!",
-            }
-        )
+        response = self._register(email="email")
         self.assertEqual(response.status_code, 400)
         self.assertFalse(User.objects.filter(email="email").exists())
 
     def test_minimum_length_password_validation(self):
         response = self._register(
-            {
-                "first_name": "Test",
-                "last_name": "Name",
-                "email": "test@example.com",
-                "password": "Pass12!",
-                "confirm_password": "Pass12!",
-            }
+            password="Pass12!",
+            confirm_password="Pass12!",
         )
         self.assertEqual(response.status_code, 400)
         self.assertFalse(User.objects.filter(email="test@example.com").exists())
 
     def test_common_password_validation(self):
         response = self._register(
-            {
-                "first_name": "Test",
-                "last_name": "Name",
-                "email": "test@example.com",
-                "password": "Password123",
-                "confirm_password": "Password123",
-            }
+            password="Password123",
+            confirm_password="Password123",
         )
         self.assertEqual(response.status_code, 400)
         self.assertFalse(User.objects.filter(email="test@example.com").exists())
 
     def test_numeric_password_validation(self):
         response = self._register(
-            {
-                "first_name": "Test",
-                "last_name": "Name",
-                "email": "test@example.com",
-                "password": "68495023",
-                "confirm_password": "68495023",
-            }
+            password="68495023",
+            confirm_password="68495023",
         )
         self.assertEqual(response.status_code, 400)
         self.assertFalse(User.objects.filter(email="test@example.com").exists())
 
     def test_removal_of_required_fields(self):
         response = self._register(
-            {
-                "first_name": "Test",
-            }
+            first_name=None,
+            last_name=None,
+            email=None,
+            password=None,
+            confirm_password=None,
         )
         self.assertEqual(response.status_code, 400)
         self.assertFalse(User.objects.filter(first_name="Test").exists())
 
-    def _register(self, payload: dict) -> DjangoResponse | DRFResponse:
+    def _register(self, **kwargs) -> DjangoResponse | DRFResponse:
+        payload = {
+            "first_name": "Test",
+            "last_name": "Name",
+            "email": "test@example.com",
+            "password": "Password123!",
+            "confirm_password": "Password123!",
+            **kwargs,
+        }
         response = self.public_client.post("/accounts/register/", data=payload, content_type="application/json")
         return response
