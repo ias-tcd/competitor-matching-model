ad226fdbe33e929b7f9d014d02b2b5bce253fe6d
Sam <sktylr@gmail.com>
2024-03-04 20:32:12 +0000

    clean up entrypoint script to not need to force prune

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-docker-compose --env-file ./.env -f docker-compose.prod.yml run api python manage.py migrate
+docker-compose --env-file ./.env -f docker-compose.prod.yml run --rm api python manage.py migrate
 docker-compose --env-file ./.env -f docker-compose.prod.yml up -d
-docker system prune -af
