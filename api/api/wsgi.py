902abc290a901ac1e994460c20cca4eb13471c16
Sam <sktylr@gmail.com>
2024-03-12 22:01:20 +0000

    try two levels

--- api/api/wsgi.py
+++ api/api/wsgi.py
@@ -13,6 +13,6 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
 application = get_wsgi_application()
