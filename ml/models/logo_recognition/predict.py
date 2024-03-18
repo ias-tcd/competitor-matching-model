19381cd70d22937b0037899dbc25f15cf7a0c798
Hanna Gorynska <127849317+Hannaxgy@users.noreply.github.com>
2024-03-18 13:54:05 +0000

    cache model for future loads

--- ml/models/logo_recognition/predict.py
+++ ml/models/logo_recognition/predict.py
@@ -1,11 +1,12 @@
 import numpy as np
 from PIL import Image
 
-from .model import model
+from .model import get_model
 from .preprocess import preprocess
 
 
 def predict(image: Image) -> np.ndarray:
+    model = get_model()
     preprocessed_image = preprocess(image)
     predictions = model.predict(preprocessed_image)
     return predictions
