e1d2c436de118206c9df5a1e963ea6f3ac1b99ca
liuphui <liup1@tcd.ie>
2024-03-21 18:03:40 +0000

    Added build.py to build the pickles and the tree for our search

---
+++ ml/models/search/build.py
@@ -0,0 +1,47 @@
+import os
+from typing import Dict
+import faiss
+import pickle
+
+from .mappy import load
+from .mappy import save
+from .store import store
+from .load import load as load_tree
+
+def build_pickles():
+    directory = os.listdir("pickles")
+    
+    try:
+        index = load_tree("/src/ml/models/search/tree.h5")
+    except:
+        index = faiss.IndexFlatL2(2048)
+        
+    brands_map = load()
+    count = find_max(brands_map)+1
+    
+    for dir in directory:
+        counts = []
+        for file in os.listdir(f"pickles/{dir}"):
+            with open(f"pickles/{dir}/{file}", 'rb') as pickle_file:
+                vector = pickle.load(pickle_file)
+            index.add(vector)
+            counts.append(count)
+            count+=1
+            
+        if (dir in brands_map):
+            brands_map[dir].extend(counts)
+        else:
+            brands_map[dir] = counts
+    save(brands_map)
+    store("/src/ml/models/search/tree.h5", index)
+        
+def find_max(brands_map: Dict) -> int:
+    indices = []
+    for v in brands_map.values():
+        indices += v
+    return max(indices)
+    
+    
+            
+            
+        
