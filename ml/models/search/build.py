08bc4d0bbbe87412a6db29ecacc6a408599a2a1a
liuphui <liup1@tcd.ie>
2024-03-21 18:06:23 +0000

    Change the paths to mappy.py and better formatting of code

--- ml/models/search/build.py
+++ ml/models/search/build.py
@@ -1,47 +1,44 @@
 import os
-from typing import Dict
-import faiss
 import pickle
+from typing import Dict
 
-from .mappy import load
-from .mappy import save
-from .store import store
+import faiss
+
 from .load import load as load_tree
+from .mappy import load, save
+from .store import store
+
 
 def build_pickles():
     directory = os.listdir("pickles")
-    
+
     try:
         index = load_tree("/src/ml/models/search/tree.h5")
     except:
         index = faiss.IndexFlatL2(2048)
-        
+
     brands_map = load()
-    count = find_max(brands_map)+1
-    
+    count = find_max(brands_map) + 1
+
     for dir in directory:
         counts = []
         for file in os.listdir(f"pickles/{dir}"):
-            with open(f"pickles/{dir}/{file}", 'rb') as pickle_file:
+            with open(f"pickles/{dir}/{file}", "rb") as pickle_file:
                 vector = pickle.load(pickle_file)
             index.add(vector)
             counts.append(count)
-            count+=1
-            
-        if (dir in brands_map):
+            count += 1
+
+        if dir in brands_map:
             brands_map[dir].extend(counts)
         else:
             brands_map[dir] = counts
     save(brands_map)
     store("/src/ml/models/search/tree.h5", index)
-        
+
+
 def find_max(brands_map: Dict) -> int:
     indices = []
     for v in brands_map.values():
         indices += v
     return max(indices)
-    
-    
-            
-            
-        
