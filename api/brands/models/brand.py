5ed9ccd872502cc4a56896adf4f1fbfa5fcc0990
Dhruv Ranajit Choudhury <111229637+dhrvrc@users.noreply.github.com>
2024-03-27 23:09:10 +0000

    update metaclass of brands and images

--- api/brands/models/brand.py
+++ api/brands/models/brand.py
@@ -7,5 +7,5 @@ class Brand(BaseModel):
     name = models.CharField(blank=False, null=False, max_length=256)
     logo = models.URLField(blank=False, null=False)
 
-    class meta:
+    class Meta:
         app_label = "brands"
