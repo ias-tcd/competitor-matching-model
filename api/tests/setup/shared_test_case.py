c9b0df0ed2cf4fd3a22da03ef433d8691381f346
Sam <sktylr@gmail.com>
2024-03-28 18:14:20 +0000

    remove user declaration

--- api/tests/setup/shared_test_case.py
+++ api/tests/setup/shared_test_case.py
@@ -1,10 +1,7 @@
-from accounts.models import User
 from django.test import TestCase
 
 
 class SharedTestCase(TestCase):
-    user: User
-
     @classmethod
     def setUpTestData(cls):
         """Initialise any test data here, such as users"""
