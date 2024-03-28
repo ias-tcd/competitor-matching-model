51ad11a70ec428c6152f0927825b80efda0df361
Sam <sktylr@gmail.com>
2024-03-28 18:06:49 +0000

    wrap in try catch

--- api/api/permissions/is_authenticated.py
+++ api/api/permissions/is_authenticated.py
@@ -8,15 +8,19 @@ from api import settings
 
 class IsAuthenticated(BasePermission):
     def has_permission(self, request, view):
-        auth_token = request.headers["Authorization"].split("Bearer ")[1]
-        auth_token = jwt.decode(auth_token, key=settings.SECRET_KEY, algorithms=[settings.SIMPLE_JWT["ALGORITHM"]])
-        expiry = auth_token["exp"]
-        token_type = auth_token["token_type"]
-        expired = timezone.now().timestamp() >= expiry
-        if expired or token_type != "access":  # nosec: B105
-            return False
-        user = get_user_model().objects.filter(id=auth_token["id"])
-        if user := user.first():
-            request.user = user
-            return True
+        try:
+            auth_token = request.headers["Authorization"].split("Bearer ")[1]
+            auth_token = jwt.decode(auth_token, key=settings.SECRET_KEY, algorithms=[settings.SIMPLE_JWT["ALGORITHM"]])
+            expiry = auth_token["exp"]
+            token_type = auth_token["token_type"]
+            expired = timezone.now().timestamp() >= expiry
+            if expired or token_type != "access":  # nosec: B105
+                return False
+            user = get_user_model().objects.filter(id=auth_token["id"])
+            if user := user.first():
+                request.user = user
+                return True
+        except (Exception,):
+            # Intentionally passing due to being handled below
+            pass
         return False
